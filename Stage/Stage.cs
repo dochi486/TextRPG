341c0f9f045869f075b8cd0779acbd91c5b61f37
Myongsu Kim <93mskim@gmail.com>
2024-02-05 23:16:51 +0900

    Stage스크립트 작업 시작

---
+++ Stage/Stage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG.Stage
+{
+    internal class Stage
+    {
+        public void BattleDialogue()
+        {
+            Console.WriteLine("무엇을 하시겠습니까?");
+            Console.WriteLine("1. 공격한다.");
+            Console.WriteLine("2. 도망간다.");
+        }
+
+    }
+}
