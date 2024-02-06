3555ec3d7e612c3edee9c4e4d7e43b9f93c7ea35
Myongsu Kim <93mskim@gmail.com>
2024-02-06 23:21:50 +0900

    몬스터 소환 함수화 작업 중

--- Dialogues/Monster/MonsterDialogue.cs
+++ Dialogues/Monster/MonsterDialogue.cs
@@ -1,6 +1,16 @@
-﻿namespace TextRPG.Dialogues.Monster;
+﻿namespace TextRPG;
 
 public class MonsterDialogue
 {
-    
+    public static Monster SpawnOrc()
+    {
+        var orcHp = Util.GetRandom(10);
+        var orcMp = Util.GetRandom(10);
+        var orcDamage = Util.GetRandom(10);
+        Monster orc = new Orc("오크", orcHp, orcMp, orcDamage);
+        Console.WriteLine($"몬스터 {nameof(orc)}가 나타났습니다!");
+        Console.WriteLine($"{nameof(orc)}의 체력은 {orc.Stat.Hp}입니다.");
+
+        return orc;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -125,12 +125,8 @@ switch(secondAction)
     case "1":
         {
             Console.WriteLine("사냥하기");
-            var orcHp = Util.GetRandom(10);
-            var orcMp = Util.GetRandom(10);
-            var orcDamage = Util.GetRandom(10);
-            Monster orc = new Orc("오크", orcHp, orcMp, orcDamage);
-            Console.WriteLine($"몬스터 {nameof(orc)}가 나타났습니다!");
-            Console.WriteLine($"{nameof(orc)}의 체력은 {orc.Stat.Hp}입니다.");
+
+            var orc = MonsterDialogue.SpawnOrc();
 
             Stage.BattleSelectDialogue();
 
