3afb4087021953442cf6a1d82fed26e7eacc881a
Myongsu Kim <93mskim@gmail.com>
2024-02-06 23:23:56 +0900

    오크 생성하는 함수 반환형 Orc로 변경

--- Dialogues/Monster/MonsterDialogue.cs
+++ Dialogues/Monster/MonsterDialogue.cs
@@ -2,12 +2,12 @@
 
 public class MonsterDialogue
 {
-    public static Monster SpawnOrc()
+    public static Orc SpawnOrc()
     {
         var orcHp = Util.GetRandom(10);
         var orcMp = Util.GetRandom(10);
         var orcDamage = Util.GetRandom(10);
-        Monster orc = new Orc("오크", orcHp, orcMp, orcDamage);
+        Orc orc = new Orc("오크", orcHp, orcMp, orcDamage);
         Console.WriteLine($"몬스터 {nameof(orc)}가 나타났습니다!");
         Console.WriteLine($"{nameof(orc)}의 체력은 {orc.Stat.Hp}입니다.");
 
