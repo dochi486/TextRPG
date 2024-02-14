f361b39d153913a79cd548eb0778d4983db7e989
김명수 <mskim@jstair.com>
2024-02-14 16:24:48 +0900

    DatabaseManager, Player, Stage 다이얼로그 모델 커밋

---
+++ Manager/DatabaseManager.cs
@@ -0,0 +1,16 @@
+﻿namespace TextRPG
+{
+    public static class DataBaseManager
+    {
+        public enum DB_MODE
+        {
+            Code,
+            Local,
+            Database
+        }
+        
+        public static DB_MODE mode = DB_MODE.Code;
+        public static string GetStageDialog(string dialog_key) => StageDialogModel.Get(dialog_key);
+        public static string GetPlayerDialog(string dialog_key) => PlayerDialogModel.Get(dialog_key);
+    }
+}
---
+++ Model/PlayerDialogModel.cs
@@ -0,0 +1,29 @@
+﻿namespace TextRPG
+{
+    public static class PlayerDialogModel
+    {
+        private static readonly Dictionary<string, string> dialog_dic = new Dictionary<string, string>()
+        {
+            ["ClassSelectDialog"] = "클래스를 선택하세요 \n" +
+                                    "1. 마법사 \n" +
+                                    "2. 전사 \n" +
+                                    "3. 궁수"
+        };
+
+        public static string Get(string dialog) => DataBaseManager.mode switch
+        {
+            DataBaseManager.DB_MODE.Code => dialog_dic[dialog],
+            DataBaseManager.DB_MODE.Database => DataFromDataBase(),
+            _ => dialog_dic[dialog]
+        };
+
+        // DB에서 가져올때의 로직
+        private static string DataFromDataBase()
+        {
+            return "클래스를 선택하세요 \n" +
+                   "1. 마법사 \n" +
+                   "2. 전사 \n" +
+                   "3. 궁수";
+        }
+    }
+}
