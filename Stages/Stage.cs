f4981bed8556a87adb1d0eb865ff9f465903c3e1
김명수 <mskim@jstair.com>
2024-02-16 10:27:05 +0900

    몬스터 전투 공격 부분 함수 추출

--- Stages/Stage.cs
+++ Stages/Stage.cs
@@ -2,7 +2,6 @@
 using TextRPG.Dialogues.Monster;
 using TextRPG.Dialogues.Player;
 using TextRPG.Dialogues.Stage;
-using TextRPG.Utils;
 
 namespace TextRPG.Stages
 {
@@ -20,41 +19,7 @@ namespace TextRPG.Stages
 
                         var playerAction = Console.ReadLine();
 
-                        switch(playerAction)
-                        {
-                            case "1":
-                                {
-                                    while(monster.Stat.Hp > 0)
-                                    {
-                                        var damage = monster.GetDamage();
-                                        player.Attack(damage);
-
-                                        if(player.Stat.Hp <= 0)
-                                        {
-                                            PlayerDialogues.PlayerDead();
-                                            break;
-                                        }
-
-                                        var playerDamage = player.GetDamage();
-                                        monster.Attack(playerDamage);
-                                        if(monster.Stat.Hp <= 0)
-                                        {
-                                            MonsterDialogue.KilledMonster(monster.Name);
-                                            player.Stat.Hp += 10;
-                                            PlayerDialogues.PlayerHPIncreased(10);
-
-                                            PlayerDialogues.ActionChoose();
-                                            break;
-                                        }
-                                    }
-                                }
-                                break;
-                            case "2":
-                                {
-                                    PlayerDialogues.PlayerRun();
-                                }
-                                break;
-                        }
+                        MonsterAttack(player, monster, playerAction);
                     }
                     break;
                 case "2":
@@ -67,6 +32,45 @@ namespace TextRPG.Stages
                     Console.WriteLine("잘못된 선택입니다.");
                 } 
                 break;
+            }
+        }
+
+        private static void MonsterAttack(Player player, Monster monster, string? playerAction)
+        {
+            switch(playerAction)
+            {
+                case "1":
+                {
+                    while(monster.Stat.Hp > 0)
+                    {
+                        var damage = monster.GetDamage();
+                        player.Attack(damage);
+
+                        if(player.Stat.Hp <= 0)
+                        {
+                            PlayerDialogues.PlayerDead();
+                            break;
+                        }
+
+                        var playerDamage = player.GetDamage();
+                        monster.Attack(playerDamage);
+                        if(monster.Stat.Hp <= 0)
+                        {
+                            MonsterDialogue.KilledMonster(monster.Name);
+                            player.Stat.Hp += 10;
+                            PlayerDialogues.PlayerHPIncreased(10);
+
+                            PlayerDialogues.ActionChoose();
+                            break;
+                        }
+                    }
+                }
+                    break;
+                case "2":
+                {
+                    PlayerDialogues.PlayerRun();
+                }
+                    break;
             }
         }
     }
